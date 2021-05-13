021f243356b764857015c879a642b98b9898a591
Jean-Pierre-A <jean.agudelo@upb.edu.co>
2021-05-12 23:57:43 -0500

    Funcion Registro

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,4 +10,47 @@ use App\User;
 class AuthController extends Controller
 {
     //
+
+    public function registro(Request $request){
+
+        $request->validate([
+            'name'=> 'required',
+            'email'=> 'required|email|unique:users',
+            'password'=>'required|min:6'
+        ]);
+
+        $user=User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $user->save();
+        $token = $user->createToken($user->email.'-'.now());
+
+        return response()->json(['user'=>$user,'token'=>$token->accessToken]);
+
+    }
+
+    public function login(Request $request){
+
+        $request->validate([
+        'email'=>'required|email|exists:users,email',
+        'password'=> 'required|min:6'
+        ]);
+        
+        if(Auth::attempt(['email'=>$request->email,'password'=>$request->password]))
+        {
+            $user=Auth::user();
+            $token = $user->createToken($user->email.'-'.now());
+            return response()->json([
+            'token'=>$token->accessToken,
+            'user'=>$user
+            ]);
+        }
+        else{
+            return response()->json([
+            'error'=>"unauthorized"
+            ]);
+        }
+    }
 }
